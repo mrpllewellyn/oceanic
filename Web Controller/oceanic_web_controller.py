c68ba662d8d1e84a60da628fc96255a79488799f
mrpllewellyn <mrpllewellyn@gmail.com>
2016-06-03 18:45:27 +0100

    inverted camera position

--- Web Controller/oceanic_web_controller.py
+++ Web Controller/oceanic_web_controller.py
@@ -44,7 +44,7 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
-#ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 os.system("killall mjpg_streamer")
 os.system("mjpg_streamer -i 'input_testpicture.so' -o 'output_http.so -w /srv/http/mjpg' &")
 time.sleep(2)
@@ -72,7 +72,7 @@ def background_thread():
         time.sleep(0.05)
         count += 1
         if something_changed == 1:
-            #sendSerial()
+            sendSerial()
             something_changed = 0
 
 
